d1cddc9140873c60efc8702ac3fdf4ead79d4b74
I'M THE DELL <I'M THE DELL@192.168.0.110>
2019-06-12 14:12:07 +0530

    saved

---
+++ src/main/java/com/app/SpringBootProfilesCode2Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfilesCode2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfilesCode2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/service/NotificationServiceImpl.java
@@ -0,0 +1,15 @@
+package com.app.service;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+@Component
+@Profile("default")
+public class NotificationServiceImpl implements INotificationService {
+
+	@Override
+	public void notificate() {
+		System.out.println("common for All");
+		
+	}
+
+}
